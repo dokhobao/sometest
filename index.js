0efffec2d28bcac15d276f568e99489d7139956a
dinhpc <dinhpc@emotiv.com>
2019-01-13 16:15:05 +0700

    asdsad

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ console.log(1)
 console.log(4)                                                                                                                                                                                                      mr cuong
 
 
-asdajkdjs
+asdajkdjssd
 asdasdas
