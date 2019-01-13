5bca0a56a3e3c5a8d5bb56bce7fcd514fbbbd7b5
dokhobao <44937765+dokhobao@users.noreply.github.com>
2019-01-13 17:12:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-console.log(1)
-dsajhdsajdsajhdsa . code moi cua tao 
-
-console.log(4)    code rat xin                                                                                                                                                                                                  mr cuong
-
-
-asdajkdjssd
-asdasdas
+sdfds
